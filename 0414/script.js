0c96ce25c5d67c23262c1d57922b980a81e1d91a
derekbl17 <derekc1fill@gmail.com>
2025-04-14 11:22:22 +0300

    0414

---
+++ 0414/script.js
@@ -0,0 +1,17 @@
+const onPageLoad = () => {
+    console.log("page loaded");
+    const getData = fetch("https://restcountries.com/v3.1/name/                               ");
+    const result = await fetch(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.wuc8GyMQAnrmyzgPP9To6wHaEH%26pid%3DApi&f=1&ipt=2dffbaa6aff94a0f492af13d96245fa79eff9f54b816ff3121f8581cbdced51d&ipo=images;.)
+    console.log(result); const container = document.getElementById("container");
+
+    result.map=document.createElement("div");
+    const card = document.createelekemt("div");
+    card.classname("card");
+
+    containner.apped(card);
+    image .src = el.urls.small
+    image.alt = el.alt_description;
+
+    card.append(image);
+
+};
